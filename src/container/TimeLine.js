641bc6b711beef7e83284eadd6e0fa2a7ada00b2
soyanakagawa <soya.nakagawa@gmail.com>
2018-07-21 16:08:56 +0900

    Add Gifted chat

--- src/container/TimeLine.js
+++ src/container/TimeLine.js
@@ -1,17 +1,55 @@
 import React from "react";
 import {StyleSheet, Text, View} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open dadadup App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
-            </View>
-        );
-    }
+
+  state = {
+    messages: [],
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 3,
+          text: 'This is a system message',
+          createdAt: new Date(Date.UTC(2016, 5, 11, 17, 20, 0)),
+          system: true,
+          // Any additional custom parameters are passed through
+        }
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
